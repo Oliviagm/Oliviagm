91bc8885bec15ff86d9b485f3b32e3314edb0e15
Andrew Sipes <andrew.n.sipes@gmail.com>
2021-04-16 15:39:02 -0400

    Fixed HTML validation issues. Formatted Prettier.

--- js/toggle.js
+++ js/toggle.js
@@ -3,27 +3,26 @@
  * help from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
  */
 
-let answers = ["Just for testing", "", "", "", "", "", "", ""]
+let answers = ["Just for testing", "", "", "", "", "", "", ""];
 
-let button = document.getElementById('b1');
-
+let button = document.getElementById("b1");
 
 button.addEventListener("click", toggle);
 
-let aID= 'a'
-let bID = 'b'
-for (i=1; i<9; i++) {
+let aID = "a";
+let bID = "b";
+for (i = 1; i < 9; i++) {
   let answerID = aID + i;
   let button = bID + i;
-    function toggle() {
-        answer = document.getElementById(answerID);
-        console.log(answer);
-        console.log(answer.style.display);
-        if (answer.style.display === "block") {
-            answer.style.display = "none";
-        } else{
-            answer.style.display = "block"
-        }
+  function toggle() {
+    answer = document.getElementById(answerID);
+    console.log(answer);
+    console.log(answer.style.display);
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
     }
-    document.getElementById(button).addEventListener("click", toggle);
+  }
+  document.getElementById(button).addEventListener("click", toggle);
 }
